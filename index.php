682eeb0ddbbfd5505b146ed95f0de531e4885eaf
ValentineOO <varlomxg@gmail.com>
2024-03-11 15:44:25 +0100

    added index page

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+
+</head>
+
+<body>
+    <h1>Home</h1>
+
+    <?php if (isset($_SESSION["user_id"])) :
+    ?>
+        <p>You are now logged in.</p>
+
+    <?php else : ?>
+
+        <p><a href="login.php">Log in</a> or <a href="signup.html">sign up</a></p>
+    <?php endif; ?>
+</body>
+
+</html>
