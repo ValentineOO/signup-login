dc470e087b7b1559e746cbbdeee89a4e8229c253
ValentineOO <varlomxg@gmail.com>
2024-03-15 15:06:56 +0100

    added more features

--- send-password-reset.php
+++ send-password-reset.php
@@ -1,4 +1,5 @@
 <?php
+
 $email = $_POST["email"];
 
 $token = bin2hex(random_bytes(16));
@@ -9,8 +10,10 @@ $expiry = date("Y-m-d H:i:s", time() + 60 * 30);
 
 $mysqli = require __DIR__ . "/database.php";
 
-$sql = "UPDATE user SET reset_token_hash = ?,
-reset_token_expires_at = ? WHERE email = ?";
+$sql = "UPDATE user
+        SET reset_token_hash = ?,
+            reset_token_expires_at = ?
+        WHERE email = ?";
 
 $stmt = $mysqli->prepare($sql);
 
@@ -19,4 +22,26 @@ $stmt->bind_param("sss", $token_hash, $expiry, $email);
 $stmt->execute();
 
 if ($mysqli->affected_rows) {
+
+    $mail = require __DIR__ . "/mailer.php";
+
+    $mail->setFrom("varlomxg@gmail.com");
+    $mail->addAddress($email);
+    $mail->Subject = "Password Reset";
+    $mail->Body = <<<END
+
+    Click <a href="http://example.com/reset-password.php?token=$token">here</a> 
+    to reset your password.
+
+    END;
+
+    try {
+
+        $mail->send();
+    } catch (Exception $e) {
+
+        echo "Message could not be sent. Mailer error: {$mail->ErrorInfo}";
+    }
 }
+
+echo "Message sent, please check your inbox.";
